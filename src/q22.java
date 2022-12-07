3200359923954e59a94224823d095ffec27ccfb8
pratham <pratham1317@gmail.com>
2022-12-08 00:36:53 +0530

    hi

--- src/q22.java
+++ src/q22.java
@@ -1,2 +1,20 @@
+import  java.util.*;
 public class q22 {
+    public static void main(String[] args) {
+        int m = 5;
+        int n = 4;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
+
+
