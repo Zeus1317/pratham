c7c0343bf428168161b61061c3ee6cd8d6af2a69
pratham <pratham1317@gmail.com>
2022-12-08 00:05:06 +0530

    hi

---
+++ src/q20.java
@@ -0,0 +1,21 @@
+
+public class q20 {
+        public static void main(String[] args) {
+                int i=1,n=20;
+                while (i<=n){
+                        if (i%2==0) {
+                                System.out.println(i + " ");
+                        }
+
+                        i++;
+
+                }
+
+
+        }
+}
+
+
+
+
+
