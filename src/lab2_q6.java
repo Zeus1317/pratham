46538ec67ddc53b04f8764a91b8073ff7f4515bf
pratham <pratham1317@gmail.com>
2022-12-09 01:42:09 +0530

    hi

--- src/lab2_q6.java
+++ src/lab2_q6.java
@@ -3,11 +3,12 @@ public class lab2_q6 {
 
     public static void main(String[] args) {
         double seconds,g=32.174,d, feet;
+        System.out.println("Enter the number of seconds");
         Scanner in = new Scanner(System.in);
         seconds = in.nextDouble();
 
         d= (0.5)*g*Math.pow(seconds,2);
-
+        System.out.println("Distance travelled");
         System.out.println(d);
 
 
